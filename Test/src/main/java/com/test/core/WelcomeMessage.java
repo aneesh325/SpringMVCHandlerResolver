bd3081ce3b1d17f509262bede26e41c8782fbcea
aneesh325 <aneeshtherocker@gmail.com>
2019-07-17 10:44:13 -0500

    Add files via upload

---
+++ Test/src/main/java/com/test/core/WelcomeMessage.java
@@ -0,0 +1,14 @@
+package com.test.core;
+
+public class WelcomeMessage {
+
+	
+	public static String greeting(){
+		return "you are in the Static method";
+	}
+	
+	public  String welcomeGreeting(){
+		return "you are in the Dynamic method";
+	}
+	
+}
